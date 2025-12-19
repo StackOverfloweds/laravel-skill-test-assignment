6b2fe9f780b32960fa8953f776c796dffe6a73ef
StackOverfloweds <bryangithub32@gmail.com>
2025-12-20 00:26:10 +0800

    add : make routes for all controller

--- laravel-skill-test/routes/api.php
+++ laravel-skill-test/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Post\PostController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/create', [PostController::class, 'create']);
+Route::middleware('auth')->post('posts', [PostController::class, 'store']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::get('posts/{id}/edit', [PostController::class, 'edit']);
+Route::middleware('auth')->put('posts/{id}', [PostController::class, 'update']);
+Route::middleware('auth')->patch('posts/{id}', [PostController::class, 'update']);
+Route::middleware('auth')->delete('posts/{id}', [PostController::class, 'destroy']);
