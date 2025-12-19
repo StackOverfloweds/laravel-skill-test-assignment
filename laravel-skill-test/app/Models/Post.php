83df438585ec3443e7fa2c847a3e80ef4e58681b
StackOverfloweds <bryangithub32@gmail.com>
2025-12-20 00:47:00 +0800

    add : fix some typo and make models

--- laravel-skill-test/app/Models/Post.php
+++ laravel-skill-test/app/Models/Post.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'is_draft',
+        'published_at',
+    ];
+
+    // Relation with user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- laravel-skill-test/routes/api.php
+++ laravel-skill-test/routes/api.php
@@ -4,11 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Post\PostController;
 
-Route::get('posts', [PostController::class, 'index']);
-Route::get('posts/create', [PostController::class, 'create']);
-Route::middleware('auth')->post('posts', [PostController::class, 'store']);
-Route::get('posts/{id}', [PostController::class, 'show']);
-Route::get('posts/{id}/edit', [PostController::class, 'edit']);
-Route::middleware('auth')->put('posts/{id}', [PostController::class, 'update']);
-Route::middleware('auth')->patch('posts/{id}', [PostController::class, 'update']);
-Route::middleware('auth')->delete('posts/{id}', [PostController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
